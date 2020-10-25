2880ea4b909c9c8279af97780c8cc74b19214a4a
Tomasz Budny <tomasz.budny.pro@gmail.com>
2020-10-25 21:39:18 +0100

    feat: chart placeholder and hardcoded transactions listing (lesson 74)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ut_personal_expenses/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,14 +14,46 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Game Pad',
+      amount: 95.00,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('Chart'),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: transactions.map((Transaction transaction) {
+              return Card(
+                child: Text(transaction.title),
+                elevation: 5,
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
