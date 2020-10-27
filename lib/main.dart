18c6cb022ec21cb8c211ecb03a10972307c6b072
Tomasz Budny <tomasz.budny.pro@gmail.com>
2020-10-27 20:05:38 +0100

    feat: [WIP][EXAMPLE] storing text input on every key press (lesson 84A)

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,9 @@ class MyHomePage extends StatelessWidget {
     ),
   ];
 
+  String titleInput;
+  String amountInput;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,12 +61,19 @@ class MyHomePage extends StatelessWidget {
                 children: [
                   TextField(
                     decoration: InputDecoration(labelText: 'Title'),
+                    onChanged: (value) {
+                      titleInput = value;
+                    },
                   ),
                   TextField(
                     decoration: InputDecoration(labelText: 'Amount'),
+                    onChanged: (value) => amountInput = value,
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      print(titleInput);
+                      print(amountInput);
+                    },
                     child: Text('Add Transaction'),
                     textColor: Colors.purple,
                   )
