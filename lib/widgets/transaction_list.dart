c4a06b35fc8f3a7f16ed11c0b6fec1451fe0e190
Tomasz Budny <tomasz.budny.pro@gmail.com>
2020-10-29 20:31:49 +0100

    feat: scrollable list with fixed height (lesson 88)

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,59 +10,63 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((Transaction transaction) {
-        return Card(
-          child: Row(
-            children: [
-              /////////// TX AMOUNT
-              Container(
-                child: Text(
-                  '\$${transaction.amount}',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.purple,
-                  ),
-                ),
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.purple,
-                    width: 2,
-                  ),
-                ),
-                padding: EdgeInsets.all(10),
-              ),
-              /////////// TX DETAILS
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  /////////// TX TITLE
-                  Text(
-                    transaction.title,
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Row(
+              children: [
+                /////////// TX AMOUNT
+                Container(
+                  child: Text(
+                    '\$${transactions[index].amount}',
                     style: TextStyle(
-                      fontSize: 16,
+                      fontSize: 20,
                       fontWeight: FontWeight.bold,
+                      color: Colors.purple,
                     ),
                   ),
-                  /////////// TX DATE
-                  Text(
-                    DateFormat.yMMMMEEEEd().format(transaction.date),
-                    style: TextStyle(
-                      color: Colors.grey,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
                     ),
-                  )
-                ],
-              )
-            ],
-          ),
-          elevation: 5,
-        );
-      }).toList(),
+                  ),
+                  padding: EdgeInsets.all(10),
+                ),
+                /////////// TX DETAILS
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    /////////// TX TITLE
+                    Text(
+                      transactions[index].title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    /////////// TX DATE
+                    Text(
+                      DateFormat.yMMMMEEEEd().format(transactions[index].date),
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+            elevation: 5,
+          );
+        },
+      ),
     );
   }
 }
