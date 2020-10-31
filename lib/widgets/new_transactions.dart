38647077afcd7a8ee9ceb0ae6033d1b8464d60d8
Tomasz Budny <tomasz.budny.pro@gmail.com>
2020-10-31 12:19:48 +0100

    style: set Add Transaction button color to theme primary

--- lib/widgets/new_transactions.dart
+++ lib/widgets/new_transactions.dart
@@ -37,7 +37,7 @@ class _NewTransactionState extends State<NewTransaction> {
             FlatButton(
               onPressed: submitData,
               child: Text('Add Transaction'),
-              textColor: Colors.purple,
+              textColor: Theme.of(context).primaryColor,
             )
           ],
         ),
